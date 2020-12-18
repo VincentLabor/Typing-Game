288201f0cef47f60f472e4d2b5e3a2d5af1b0430
VincentLabor <vlabor95@gmail.com>
2020-12-17 16:40:27 -0800

    Difficulties affect word

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 let diffSetting = document.getElementById("difficultySetting");
 let difftext = document.getElementById("diffText");
 let textBar = document.getElementById("textInput");
+let actualDiff = document.getElementById("difficultyValue");
+let wordToBeTyped = document.getElementById("word");
 
+let removeDiffText = () => {
+  difftext.innerHTML = "";
+};
 
-let removeDiffText = () =>{
-    difftext.innerHTML = ""
+let wordList = "Poggers";
+
+let clearWindow = () => {
+  //This clears the html from the window.
+  difftext.innerHTML = "";
+  diffSetting.innerHTML = "";
+};
+
+let chosenDifficulty = (diff) =>{
+    console.log(diff);
+    if(diff === "Easy"){
+        wordToBeTyped.innerHTML = wordList;
+    } else if(diff === "Medium"){
+        wordToBeTyped.innerHTML = "MediumPoggers"
+    } else {
+        wordToBeTyped.innerHTML = "HardPogoPoggers"
+    }
 }
 
-diffSetting.addEventListener('submit',e=>{
-    e.preventDefault();
-    console.log("difficulty has been selected");
-    //If this difficulty is selected say this for all 3 versions 
-    //to ensure i make it with 3 versions. This to then become
-    // referring to functions that play specific words. 
-    difftext.innerHTML = "";
-    diffSetting.innerHTML ="";
-    textBar.removeAttribute("hidden");
-})
+diffSetting.addEventListener("submit", (e) => {
+  e.preventDefault();
+  clearWindow();
+  //This returns the difficulty setting necessary
+  chosenDifficulty(actualDiff.value)
+  textBar.removeAttribute("hidden");
+  
+});
