faec17516d1dbfc7207932b35c620602ee07bf9c
VincentLabor <vlabor95@gmail.com>
2020-12-18 13:39:19 -0800

    Input detects correct text

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let difftext = document.getElementById("diffText");
 let textBar = document.getElementById("textInput");
 let actualDiff = document.getElementById("difficultyValue");
 let wordToBeTyped = document.getElementById("word");
+let logo = document.getElementById("logo");
+let stopwatch = document.getElementById("timer");
 
 let removeDiffText = () => {
   difftext.innerHTML = "";
@@ -10,28 +12,43 @@ let removeDiffText = () => {
 
 let wordList = "Poggers";
 
+//This clears the html from the window.
 let clearWindow = () => {
-  //This clears the html from the window.
   difftext.innerHTML = "";
   diffSetting.innerHTML = "";
 };
 
-let chosenDifficulty = (diff) =>{
-    console.log(diff);
-    if(diff === "Easy"){
-        wordToBeTyped.innerHTML = wordList;
-    } else if(diff === "Medium"){
-        wordToBeTyped.innerHTML = "MediumPoggers"
-    } else {
-        wordToBeTyped.innerHTML = "HardPogoPoggers"
-    }
-}
+//This will return to orginal window after a while.
+let returnToHome = () => {
+  setTimeout(() => {
+    logo.click();
+  }, 10000);
+};
+
+//This changes words depending on diffilculty selected
+let chosenDifficulty = (diff) => {
+  console.log(diff);
+  if (diff === "Easy") {
+    wordToBeTyped.innerHTML = wordList;
+  } else if (diff === "Medium") {
+    wordToBeTyped.innerHTML = "MediumPoggers";
+  } else {
+    wordToBeTyped.innerHTML = "HardPogoPoggers";
+  }
+};
 
 diffSetting.addEventListener("submit", (e) => {
   e.preventDefault();
   clearWindow();
-  //This returns the difficulty setting necessary
-  chosenDifficulty(actualDiff.value)
+  //have to add parameter of chosen diff value.
+  chosenDifficulty(actualDiff.value);
   textBar.removeAttribute("hidden");
-  
+  returnToHome();
+});
+
+textBar.addEventListener("input", (e) => {
+  let inputtedText = e.target.value;
+  if(inputtedText === wordToBeTyped.innerHTML){
+    console.log("Bingo")
+  }
 });
