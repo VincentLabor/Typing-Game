7a838e2ed9044a794ee5d3b57f638766a7227015
VincentLabor <vlabor95@gmail.com>
2021-01-01 14:42:39 -0800

    User now sees correct letters appear

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ let actualDiff = document.getElementById("difficultyValue");
 let wordToBeTyped = document.getElementById("word");
 let logo = document.getElementById("logo");
 let stopwatch = document.getElementById("timer");
+let userScore = document.getElementById("score");
+let span = document.getElementById("showMe");
 
 let removeDiffText = () => {
   difftext.innerHTML = "";
 };
 
 let wordList = "Poggers";
+
+let usersScore = 0;
 
 //This clears the html from the window.
 let clearWindow = () => {
@@ -19,36 +23,83 @@ let clearWindow = () => {
 };
 
 //This will return to orginal window after a while.
-let returnToHome = () => {
+let returnToHomeTimer = () => {
   setTimeout(() => {
     logo.click();
-  }, 10000);
+  }, 20000);
+};
+
+let returnToHome = () => {
+  logo.click();
 };
 
 //This changes words depending on diffilculty selected
 let chosenDifficulty = (diff) => {
-  console.log(diff);
   if (diff === "Easy") {
     wordToBeTyped.innerHTML = wordList;
   } else if (diff === "Medium") {
-    wordToBeTyped.innerHTML = "MediumPoggers";
+    wordToBeTyped.innerHTML = "Where is your precious light now, Uther?";
   } else {
-    wordToBeTyped.innerHTML = "HardPogoPoggers";
+    wordToBeTyped.innerHTML =
+      "Do not speak to me! You may look like Arthas, but you are nothing like him!";
   }
 };
 
+//Checks what difficulty is selected and shows diff HTML based on difficulty selected
 diffSetting.addEventListener("submit", (e) => {
   e.preventDefault();
   clearWindow();
   //have to add parameter of chosen diff value.
   chosenDifficulty(actualDiff.value);
   textBar.removeAttribute("hidden");
-  returnToHome();
+  returnToHomeTimer();
 });
 
+let areWordsCorrect = (userTxt, txtTobeTyped) => {
+  // userTxt = userTxt.split(" ");
+  // txtTobeTyped = txtTobeTyped.split(" ");
+
+  let charVal = 0;
+
+  for (let i = 0; i < userTxt.length; i++) {
+    if (userTxt[i] === txtTobeTyped[i]) {
+      // wordToBeTyped.style.color = "green";
+      charVal++;
+      span.innerHTML ='<span class="green">' + userTxt.substring(0,charVal) + '</span>';
+    }
+  }
+};
+
+//Checks that every character is properly typed
+let checkText = (userTypedText, textToBeTyped) => {
+  if (userTypedText === textToBeTyped) {
+    returnToHome();
+  }
+};
+
+//Checks if user typed the text properly
 textBar.addEventListener("input", (e) => {
   let inputtedText = e.target.value;
-  if(inputtedText === wordToBeTyped.innerHTML){
-    console.log("Bingo")
-  }
+  let charVal = 0;
+
+  checkText(inputtedText, wordToBeTyped.innerHTML);
+
+  areWordsCorrect(inputtedText, wordToBeTyped.innerHTML;
+
+  // for(let i=0; i<wordToBeTyped; i++){
+  //   if(inputtedText[i] === wordToBeTyped[i] ){
+  //     span.innerHTML = "e.target.value.substring(0,charVal)";
+  //   }
+  // }
+
+  // span.innerHTML = e.target.value.substring(0,1) + '<span class="green">' + e.target.value.substring(1,20) + '</span>';
 });
+
+// if(checkText(inputtedText, wordToBeTyped.innerHTML) === true){
+//   userScore.innerHTML = usersScore++;
+// }
+
+// if (inputtedText === wordToBeTyped.innerHTML) {
+//   // returnToHome();
+//   userScore.innerHTML = usersScore++;
+// }
